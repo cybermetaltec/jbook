02f9aebf08de861f8b30d1a66ad4ab8afd6844cc
tongchuan <cybermetal@163.com>
2022-06-18 16:33:24 +0800

    save

--- Algorithm/42.py
+++ Algorithm/42.py
@@ -16,7 +16,25 @@ class Solution:
         return ans
 
 
-# nums = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+class Solution2:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        n = len(height)
+        leftMax = [height[0]] + [0] * (n - 1)
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+
+        rightMax = [0] * (n - 1) + [height[n - 1]]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+
+        ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+        return ans
+
+
+nums = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 # nums = [4,2,0,3,2,5]
-nums = [1]
-print(Solution().trap(nums))
+# nums = [1]
+print(Solution2().trap(nums))
