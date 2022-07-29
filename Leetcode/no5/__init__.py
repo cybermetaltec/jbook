5375395c8c97ed0d810732cfe4ce47860cf7a002
tongchuan <cybermetal@163.com>
2022-07-29 18:50:44 +0800

    save

--- Leetcode/no5/__init__.py
+++ Leetcode/no5/__init__.py
@@ -1,3 +1,24 @@
 class Solution:
+    def _lp(self, s: str, l: int, r: int) -> str:
+        ret = ""
+        while l >= 0 and r < len(s):
+            if s[l] == s[r]:
+                ret = s[l:r + 1]
+                l -= 1
+                r += 1
+            else:
+                return ret
+        return ret
+
     def longestPalindrome(self, s: str) -> str:
-        pass
+        ret = ""
+        for i in range(len(s)):
+            item_center = self._lp(s, i, i)
+            blank_center = self._lp(s, i, i + 1)
+            r = item_center if len(item_center) > len(blank_center) else blank_center
+            if len(r) > len(ret): ret = r
+        return ret
+
+
+print(Solution().longestPalindrome("babad"))
+print(Solution().longestPalindrome("cbbd"))
